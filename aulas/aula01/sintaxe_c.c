5fb31498e435e4a52d80f17c1efacb1b8a1faebb
henrique929 <henriquebruner@gmail.com>
2024-08-15 00:47:51 +0000

    matematica

---
+++ aulas/aula01/sintaxe_c.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+  int primeiro_numero = 10;
+  int segundo_numero = 20;
+  int soma = primeiro_numero + segundo_numero;
+  printf("a soma de %i com %i eh %i\n", primeiro_numero, segundo_numero,soma);
+    
+  return 0;
+}
